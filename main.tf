7e358104afd4eb525226d326bc2fe6a6082b65f2
itamar-glitch <itamarazran052@gmail.com>
2025-01-30 13:04:09 +0200

    Updated Terraform configuration with new outputs

--- main.tf
+++ main.tf
@@ -1,12 +1,26 @@
-provider "aws"{
-    region = "us-east-1"
-    version="~> 3.0"
-}
-resource "aws_instance" "example"{
-    ami = var.client_ami
-    instance_type = var.client_server
-
-    tags= {
-        Name = "Itamar_instance"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = var.client_ami
+  instance_type = var.client_server
+
+  tags = {
+    Name = "Itamar_instance"
+  }
+}
+
+output "instance_public_IP" {
+  description = "Instance public IP"
+  value       = aws_instance.example.public_ip
 }
